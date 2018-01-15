c827c58b9a1225c6b17f9c4d355dd93d64143035
sunhengchao <1029651026@qq.com>
2018-01-15 15:44:23 +0800

    init

---
+++ app/src/main/java/com/uuch/android_zxinglibrary/SecondActivity.java
@@ -0,0 +1,110 @@
+package com.uuch.android_zxinglibrary;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.uuzuche.lib_zxing.activity.CaptureFragment;
+import com.uuzuche.lib_zxing.activity.CodeUtils;
+
+import static com.uuch.android_zxinglibrary.MainActivity.REQUEST_CAMERA_PERM;
+import static com.uuch.android_zxinglibrary.MainActivity.REQUEST_IMAGE;
+
+/**
+ * 定制化显示扫描界面
+ */
+public class SecondActivity extends BaseActivity {
+
+    private CaptureFragment captureFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        captureFragment = new CaptureFragment();
+        // 为二维码扫描界面设置定制化界面
+        CodeUtils.setFragmentArgs(captureFragment, R.layout.my_camera);
+        captureFragment.setAnalyzeCallback(analyzeCallback);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fl_my_container, captureFragment).commit();
+
+        initView();
+    }
+
+    public static boolean isOpen = false;
+
+    private void initView() {
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.flashLightLayout);
+        LinearLayout albumLayout = (LinearLayout) findViewById(R.id.albumLayout);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isOpen) {
+                    CodeUtils.isLightEnable(true);
+                    isOpen = true;
+                } else {
+                    CodeUtils.isLightEnable(false);
+                    isOpen = false;
+                }
+
+            }
+        });
+        albumLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_IMAGE);
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mOnResultCallback.result(requestCode,resultCode,data);
+        finish();
+    }
+
+
+
+
+    /**
+     * 二维码解析回调函数
+     */
+    CodeUtils.AnalyzeCallback analyzeCallback = new CodeUtils.AnalyzeCallback() {
+        @Override
+        public void onAnalyzeSuccess(Bitmap mBitmap, String result) {
+            Intent resultIntent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putInt(CodeUtils.RESULT_TYPE, CodeUtils.RESULT_SUCCESS);
+            bundle.putString(CodeUtils.RESULT_STRING, result);
+            resultIntent.putExtras(bundle);
+            SecondActivity.this.setResult(RESULT_OK, resultIntent);
+            SecondActivity.this.finish();
+        }
+
+        @Override
+        public void onAnalyzeFailed() {
+            Intent resultIntent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putInt(CodeUtils.RESULT_TYPE, CodeUtils.RESULT_FAILED);
+            bundle.putString(CodeUtils.RESULT_STRING, "");
+            resultIntent.putExtras(bundle);
+            SecondActivity.this.setResult(RESULT_OK, resultIntent);
+            SecondActivity.this.finish();
+        }
+    };
+    private static OnResultCallback mOnResultCallback;
+    public static void SetOnResultCallback(OnResultCallback callback){
+        mOnResultCallback = callback;
+    }
+    interface OnResultCallback{
+        void result(int requestCode, int resultCode, Intent data);
+    }
+}
